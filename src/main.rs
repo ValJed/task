c5e05591433e6018aab4388aa8f900634ac01634
Jed <vjeudy@protonmail.com>
2023-01-28 00:33:52 +0100

    adds function to get data from file or to create it

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use chrono::{DateTime, NaiveDateTime, Utc};
+use serde::{Serialize, Deserialize};
 
 use std::env;
 use std::fs::{create_dir, File};
+use std::io::Write;
 use std::path::Path;
+use std::io::BufReader;
 
 const COMMANDS: [&str; 4] = ["add", "del", "ls", "done"];
-
 const HELP_SIGNS: [&str; 2] = ["--help", "help"];
 
+#[derive(Debug, Serialize, Deserialize)]
 struct Task {
     done: bool,
     creation_date: String,
@@ -16,8 +19,6 @@ struct Task {
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
-    println!("{:?}", args.len());
 
     if args.len() < 2 {
         println!("Explain how to use taks");
@@ -30,25 +31,34 @@ fn main() {
     //     "done" => done(),
     // }
 
-    get_or_create_file();
-    println!("Hello, world!");
+    get_or_create_data_file();
 }
 
-fn get_or_create_file() {
-    let folder_path = "~/.local/share/tasks";
-    let file_path = "~/.local/share/tasks/tasks.json";
-    let folder_exists = Path::new(folder_path).exists();
-    let file_exists = Path::new(file_path).exists();
-
-    if !folder_exists {
+fn get_or_create_data_file() -> Vec<Task> {
+    let user = env::var("USER").expect("No user set on this machine");
+    let to_folder = format!("/home/{user}/.local/share/tasks");
+    let to_file = format!("{to_folder}/tasks.json");
+    let folder_path = Path::new(to_folder.as_str());
+    let file_path = Path::new(to_file.as_str());
+    
+    if !folder_path.exists() {
         create_dir(folder_path).expect("Error when creating folder tasks");
     }
 
-    if !file_exists {
-        File::create(file_path).expect("Error when creating file tasks.json");
-    }
+    if !file_path.is_file() {
+        let mut file = File::create(file_path)
+            .expect("Error when creating file tasks.json");
+        file.write("[]".as_bytes()).expect("Error when writting to file");
 
-    // File::create(path).expect("Error when creating data file");
+        return Vec::new();
+    };
+
+    let file = File::open(file_path).expect("Error when opening file");
+    let reader = BufReader::new(file);
+    let tasks: Vec<Task> = serde_json::from_reader(reader)
+        .expect("Error when extracting data from file");
+
+    tasks
 }
 
 // fn add_task () {
